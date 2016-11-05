b53b8aaa46a752a969fc108f7c389e2cd339c310
Risto Keravuori <risto.k@mac.com>
2016-11-05 13:38:45 -0700

    Switched generators (test helpers) to actually use a generator

--- test/generators.js
+++ test/generators.js
@@ -1,24 +1,30 @@
 const RAND_MAX = 99;
 
+function* _randomNumber (count, randMax = RAND_MAX, randMin = 0) {
+  for (let i = 0; i < count; i++) {
+    yield randMin + Math.round(Math.random() * (randMax-randMin));
+  }
+}
+
 function randomNumber (randMax = RAND_MAX, randMin = 0) {
-  return randMin + Math.round(Math.random() * (randMax-randMin));
+  return [..._randomNumber(1, randMax, randMin)][0];
 }
 
 function randomArray (size, randMax, randMin) {
-  return [...Array(size)].map(randomNumber.bind(undefined, randMax, randMin)).sort();
+  return [..._randomNumber(size, randMax, randMin)].sort( (a, b) => a - b );
 }
 
 function arrayWithoutTargetValue (size, target, randMax, randMin) {
-  return [...Array(size)].map( () => {
-    for (var value = target ; value === target; value = randomNumber(randMax, randMin));
+  return [..._randomNumber(size, randMax, randMin)].map( () => {
+    for (var value = target; value === target; value = randomNumber(randMax, randMin));
     return value;
-  }).sort();
+  }).sort( (a, b) => a - b );
 }
 
-function arrayWithTargetValueAtPosition (size, target, position, limit) {
+function arrayWithTargetValueAtPosition (size, target, position, randMax) {
   const array = arrayWithoutTargetValue(position, target, target-1);
   array.push(target);
-  return array.concat(arrayWithoutTargetValue(size-position-1, target, RAND_MAX, target+1));
+  return array.concat(arrayWithoutTargetValue(size-position-1, target, randMax, target+1));
 }
 
 module.exports = {
